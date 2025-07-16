aae5c5d3fb69a99b840a5714f16b0b715226e630
Matthew Collier <collier.matthew@gmail.com>
2025-07-16 14:23:36 -0400

    Add helm provider.

---
+++ versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  # terraform v1.5 ships on google cloud shell
+  required_version = ">= 1.5"
+  required_providers {
+   google = {
+      source  = "hashicorp/google"
+      version = ">= 6.43.0"
+    }    
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 3.0.2"
+    }
+    # random = {
+    #   source  = "hashicorp/random"
+    #   version = ">= 3.6.0"
+    # }
+  }
+}
