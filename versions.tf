0c9e98eae0db1ab814bc5f24c7371237a2d8e206
Matthew Collier <collier.matthew@gmail.com>
2025-07-16 16:55:41 -0400

    Run terraform fmt.

--- versions.tf
+++ versions.tf
@@ -2,10 +2,10 @@ terraform {
   # terraform v1.5 ships on google cloud shell
   required_version = ">= 1.5"
   required_providers {
-   google = {
+    google = {
       source  = "hashicorp/google"
       version = ">= 6.43.0"
-    }    
+    }
     helm = {
       source  = "hashicorp/helm"
       version = ">= 3.0.2"
